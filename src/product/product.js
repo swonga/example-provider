44c76aa3590e4d9c6cecaa632fda466bb790dc60
swonga <111727042+swonga@users.noreply.github.com>
2024-09-12 13:37:43 +1000

    Update product.js

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,10 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, price) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
+        this.price - price;
     }
 }
 
